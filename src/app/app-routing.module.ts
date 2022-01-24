fdfa4767aec5f71ec68dd527920327789c82eb20
masete <nicholasmasete72@gmail.com>
2022-01-24 06:16:45 +0300

    created a routing testing file, and a router outlet placeholder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { FourthComponent } from './fourth/fourth.component';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'first', component: FirstComponent},
+  {path:'second', component: SecondComponent},
+  {path:'third', component: ThirdComponent},
+  {path:'fourth', component: FourthComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
