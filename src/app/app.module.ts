feb3fa1b14b6e8c31b9acc2fc8ed84070a4f6cea
masete <nicholasmasete72@gmail.com>
2022-01-24 05:55:19 +0300

    added four components to demonstrate routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,20 @@ import { StorageService } from './storage.service';
 import { TruncatePipe } from './truncate.pipe';
 import { HttpClientModule } from '@angular/common/http';
 import { UserComponent } from './user/user.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
+import { FourthComponent } from './fourth/fourth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TruncatePipe,
-    UserComponent
+    UserComponent,
+    FirstComponent,
+    SecondComponent,
+    ThirdComponent,
+    FourthComponent
   ],
   imports: [
     BrowserModule,
